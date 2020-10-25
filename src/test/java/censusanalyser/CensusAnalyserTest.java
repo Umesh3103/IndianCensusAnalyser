214eb278cf14c87bc637e556c7242569609009ed
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-25 18:47:30 +0530

    [Umesh]Added: test code to test sorting by population

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -158,7 +158,7 @@ public class CensusAnalyserTest {
 		} catch (CensusAnalyserException e) {
 		}
 	}
-	
+
 	@Test
 	public void givenIndianCensusData_WhenSortedOnPopulation_ShouldReturnSortedResult() {
 		try {
@@ -170,5 +170,16 @@ public class CensusAnalyserTest {
 		} catch (CensusAnalyserException e) {
 		}
 	}
-	
+
+	@Test
+	public void givenIndianCensusData_WhenSortedOnPopulationDensity_ShouldReturnSortedResult() {
+		try {
+			CensusAnalyser censusAnalyser = new CensusAnalyser();
+			censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			String sortedCensusData = censusAnalyser.getPopulationDensityWiseSortedCensusData();
+			IndiaCensusCSV[] censusCSV = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
+			Assert.assertEquals("Bihar", censusCSV[0].state);
+		} catch (CensusAnalyserException e) {
+		}
+	}
 }
